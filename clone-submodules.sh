0fe7c3114e9d181a70ab695073f2ec8f6ba03638
Pratik Deshmukh <86339747+pratikdeshmukh570@users.noreply.github.com>
2025-10-07 13:34:05 +0530

    Update clone-submodules.sh

--- clone-submodules.sh
+++ clone-submodules.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 set -e
-
-# Initialize and pull the core submodule
 git submodule update --init --recursive --depth 1 core || true
-
 echo "Submodule contents (core):"
-ls -la core
+ls -la core || true
 ls -la core/hdi || true
